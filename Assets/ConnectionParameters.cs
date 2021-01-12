0bd016d9fdd8f45e3e6441770f54107cf1091545
Alfredo <alfredog92@gmail.com>
2021-01-12 01:12:41 +0100

    fix connection parameters

--- Assets/ConnectionParameters.cs
+++ Assets/ConnectionParameters.cs
@@ -3,5 +3,7 @@ public class ConnectionParameters
     public static readonly string username = "ugezag";
     public static readonly string password = "fakwah";
     public static readonly string url = "local.veracity.it:3005/demo-xapi/xapi/statements";
+    public static readonly string uri = "http://local.veracity.it:3005/demo-xapi/xapi/";
+
 
 }
