b20944618ca1c661215b363999dc9a90417898e7
Alfredo <alfredog92@gmail.com>
2021-01-19 20:49:42 +0100

    new parameter files

---
+++ Assets/xAPI/UserInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo 
+{
+
+    public static string name = "Matteo Bruno";
+    public static string mail = "matteobruno@example.com";
+    public static string activityID = "http://www.example.com/unityGame";
+    public static string activityDefinition = "Game example in unity";
+}
